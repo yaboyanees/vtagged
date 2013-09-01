334210b904db0cd930165ce656745591b4d4c3e3
yaboyanees <a.merzi@yahoo.com>
2013-09-01 18:16:36 -0500

    switched yield order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,19 +11,10 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery-ui
 //= require jquery_ujs
-//= require jquery-1.7.1.min.js 
-//= require jquery-ui-1.8.16.custom.min.js
-//= require jiffies.js
 //= require jquery.purr
 //= require best_in_place
-//= require gmaps4rails/all_apis.js
-//= require gmaps4rails/bing.js
 //= require gmaps4rails/gmaps4rails.base.js.coffee
-//= require gmaps4rails/gmaps4rails.bing.js.coffee
 //= require gmaps4rails/gmaps4rails.googlemaps.js.coffee
 //= require gmaps4rails/gmaps4rails.base.js
-//= require gmaps4rails/googlemaps.js
-//= require gmap3.min.js
 //= require_tree .
